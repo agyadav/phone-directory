e36502a89a85e483614c3eb7131e8f2e64a3582f
anand <anayadav@adobe.com>
2020-12-10 23:18:03 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
 function App() {
-  return (
-    <div>
-Phone Directory
-    </div>
-  );
+    return (
+        <div>
+            {/* <div>
+                <div className="header">
+                Phone Directory
+
+            </div>
+            <button>Add</button>
+            <div>
+                <span>Name</span><br/>
+                <span>Phone</span>
+            </div> */}
+            <label htmlFor="name">Name:</label>
+            <input id="name" type="text" placeholder="Type Here" defaultValue="Shilpa"/>
+        </div>
+
+    );
 }
+
 
 export default App;
